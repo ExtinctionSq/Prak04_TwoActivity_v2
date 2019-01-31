9dbd6c726eba4599e9d3c4f7446c0df1af55fd2d
extinctionsq <jasonmiracle56@gmail.com>
2019-01-31 18:13:13 +0800

    Commit ver 3.0

--- app/src/main/java/com/example/twoactivity/MainActivity.java
+++ app/src/main/java/com/example/twoactivity/MainActivity.java
@@ -5,18 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private EditText mMessageEditText;
 
+    public static final String EXTRA_MESSAGE="com.example.twoActivity.extra.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mMessageEditText = findViewById(R.id.editText_main);
     }
 
     public void launchSecondActivity(View view) {
         Intent intent = new Intent(this, SecondActivity.class);
+        String message = mMessageEditText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE,message);
         Log.d(LOG_TAG, "button is Clicked");
         startActivity(intent);
     }
